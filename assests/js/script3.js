c86a496ffa98f6055a99bd1fce3ef576ee604fd5
ItsDr3ams <2005sancheztomas@gmail.com>
2025-06-03 11:24:47 -0400

    commit final
    .

--- assests/js/script3.js
+++ assests/js/script3.js
@@ -1,2 +1,13 @@
 //telefono debe tener exactamente 8 digitos, no es obligatoria
 //contrasenia debe tener mas de 5 caracteres, es obligatoria
+
+let telefono = []
+    function validar ({
+        let eTelefono = document.getElementById("telefono"),
+        let vTelefono = eTelefono.value,
+        let eErrorPassword = document.getElementById("errorcontrasenia"),
+
+        
+    })
+
+    
